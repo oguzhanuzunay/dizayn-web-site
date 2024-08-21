3e1527d6c3c106bf2018bb0f9af4d29da43876fc
Oğuzhan Uzunay <oguzhan.uzunay@outlook.com>
2024-08-21 17:16:38 +0300

    delete: useLocalProduct

--- constants/useLocaleProductData.tsx
+++
@@ -1,9 +0,0 @@
-export default function useLocaleLabel() {
-  const t = useLocaleLabel('useLocaleLabel');
- 
-  function getLocaleLabel(locale: 'tr' | 'en') {
-    return t('label', {locale});
-  }
- 
-  return getLocaleLabel;
-}
