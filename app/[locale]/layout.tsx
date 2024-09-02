30398b5a110328949809c5ccc738314e9c1ab197
Oguzhan <oguzhanzny@gmail.com>
2024-09-03 02:43:47 +0300

    add:Web Analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { languageTexts } from '@/constants';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
+
 import Footer from '@/components/shared/Footer';
 import { Metadata } from 'next';
 // eslint-disable-next-line camelcase
