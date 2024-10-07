ff107e17c45eca9ac05491521933090649e202a4
Oğuzhan Uzunay <oguzhan.uzunay@outlook.com>
2024-10-07 16:59:31 +0300

    add: google tag manager integration

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -10,6 +10,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 // eslint-disable-next-line camelcase
 import { Quicksand, Red_Hat_Display, Roboto, Space_Grotesk } from 'next/font/google';
+import Script from 'next/script';
 import React from 'react';
 import './globals.css';
 
@@ -46,9 +47,34 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TZC3LD3');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${roboto.className} ${spaceGrotesk.className} ${redHatDisplay.className} ${quicksand.className}`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TZC3LD3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Analytics />
         <SpeedInsights />
         <NextIntlClientProvider messages={messages}>
