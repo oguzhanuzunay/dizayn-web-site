631a27f394c47d1136c30126ec35b5e518ae65e5
Oğuzhan Uzunay <oguzhan.uzunay@outlook.com>
2025-04-09 16:27:09 +0300

    map compoonent never defined

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import Hero from '@/components/Carousel/HeroSlider';
 import CountUpComp from '@/components/CountUpComp';
-import MapComponent from '@/components/MapComponent/MapComponent';
 import NewTechPipes from '@/components/NewTechPipes';
 import Popup from '@/components/PopUp/PopUp';
 import { useEffect, useState } from 'react';
